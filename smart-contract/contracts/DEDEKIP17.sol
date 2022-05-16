98a69ac6fa5bcca24342426b193505a2bde203ab
Robin Hanjun Kwon <khj02021@gmail.com>
2022-05-16 17:01:15 +0900

    Implement KIP-7 and KIP-17

--- smart-contract/contracts/DEDEKIP17.sol
+++ smart-contract/contracts/DEDEKIP17.sol
@@ -1,90 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.5.6;
 
-import "@klaytn/contracts/token/KIP17/KIP17Full.sol";
+import "@klaytn/contracts/token/KIP17/KIP17Token.sol";
+import "./Ownable.sol";
+import "@klaytn/contracts/drafts/Counters.sol";
+import "./DEDEKIP7.sol";
 
-contract Ownable {
-    address payable private _owner;
-
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-    /**
-     * @dev Initializes the contract setting the deployer as the initial owner.
-     */
-    constructor () internal {
-        _owner = msg.sender;
-        emit OwnershipTransferred(address(0), _owner);
-    }
-
-    /**
-     * @dev Returns the address of the current owner.
-     */
-    function owner() public view returns (address payable) {
-        return _owner;
-    }
-
-    /**
-     * @dev Throws if called by any account other than the owner.
-     */
-    modifier onlyOwner() {
-        require(isOwner(), "Ownable: caller is not the owner");
-        _;
-    }
-
-    /**
-     * @dev Returns true if the caller is the current owner.
-     */
-    function isOwner() public view returns (bool) {
-        return msg.sender == _owner;
-    }
-
-    /**
-     * @dev Leaves the contract without owner. It will not be possible to call
-     * `onlyOwner` functions anymore. Can only be called by the current owner.
-     *
-     * > Note: Renouncing ownership will leave the contract without an owner,
-     * thereby removing any functionality that is only available to the owner.
-     */
-    function renounceOwnership() public onlyOwner {
-        emit OwnershipTransferred(_owner, address(0));
-        _owner = address(0);
-    }
-
-    /**
-     * @dev Transfers ownership of the contract to a new account (`newOwner`).
-     * Can only be called by the current owner.
-     */
-    function transferOwnership(address payable newOwner) public onlyOwner {
-        _transferOwnership(newOwner);
-    }
-
-    /**
-     * @dev Transfers ownership of the contract to a new account (`newOwner`).
-     */
-    function _transferOwnership(address payable newOwner) internal {
-        require(newOwner != address(0), "Ownable: new owner is the zero address");
-        emit OwnershipTransferred(_owner, newOwner);
-        _owner = newOwner;
-    }
-}
-
-contract DEDEKIP17 is KIP17Full, Ownable {
+contract DEDEKIP17 is KIP17Token, Ownable {
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
+  DEDEKIP7 token;
 
-  constructor() public KIP17Full("DEDE NFT", "DEDENFT") {}
+  constructor() public KIP17Token("DEDE NFT", "DEDENFT") {
+  }
 
-  function mintNFT(string memory tokenURI)
-  public
-  onlyOwner
-  returns (uint256)
-  {
+  function mintNFT(address recipient, string memory tokenURI, uint256 nftPrice) public onlyOwner returns(uint256) {
+	require(token.balanceOf(recipient) > nftPrice, "DEDEKIP17: Insufficient tokens to mint NFT");
+
+	token.delegatedTransferFrom(recipient, msg.sender, nftPrice);
+
 	_tokenIds.increment();
-
 	uint256 newItemId = _tokenIds.current();
-	_mint(msg.sender, newItemId);
+	_mint(recipient, newItemId);
 	_setTokenURI(newItemId, tokenURI);
 
 	return newItemId;
+  }
+
+  function setToken(address tokenAddress) public onlyOwner returns(bool) {
+	require(tokenAddress != address(0x0));
+	token = DEDEKIP7(tokenAddress);
+	return true;
   }
 }
---
+++ smart-contract/contracts/Ownable.sol
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.5.6;
+
+contract Ownable {
+    address payable private _owner;
+	address private _kip17;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor () internal {
+        _owner = msg.sender;
+        emit OwnershipTransferred(address(0), _owner);
+    }
+
+    /**
+     * @dev Throws if called by any account other than the KIP-17 smart contract account.
+     */
+	modifier onlyKip17() {
+	  require(isKip17(), "Ownable: caller is not the kip17");
+	  _;
+	}
+
+    /**
+     * @dev Returns true if the caller is the current KIP-17 smart contract account.
+     */
+	function isKip17() public view returns(bool) {
+	  return msg.sender == _kip17;
+	}
+
+	/**
+	  * @dev Set KIP-7 smart contract account by owner.
+	  */
+	function setKip17(address kip17) public onlyOwner {
+	  _kip17 = kip17;
+	}
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view returns (address payable) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(isOwner(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    /**
+     * @dev Returns true if the caller is the current owner.
+     */
+    function isOwner() public view returns (bool) {
+        return msg.sender == _owner;
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * > Note: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public onlyOwner {
+        emit OwnershipTransferred(_owner, address(0));
+        _owner = address(0);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address payable newOwner) public onlyOwner {
+        _transferOwnership(newOwner);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     */
+    function _transferOwnership(address payable newOwner) internal {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
+    }
+}
