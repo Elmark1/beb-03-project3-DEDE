d24eb3b1ad7f1c036d0ba9297d2be9d9c1f9459a
Robin Hanjun Kwon <khj02021@gmail.com>
2022-05-23 10:07:18 +0900

    Implement execution of smart contract function

--- server/src/routers/adminRouter.js
+++ server/src/routers/adminRouter.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import {createAdmin, deployContracts} from '../controllers/adminController.js';
+import {createAdmin, deployContracts, buyNft} from '../controllers/adminController.js';
 
 const adminRouter = express.Router();
 
 adminRouter.post('/admins', createAdmin);
 adminRouter.post('/deploy', deployContracts);
+adminRouter.post('/nfts', buyNft);
 
 export default adminRouter;
