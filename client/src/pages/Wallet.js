bb9d567fdaeaa7a6fe02db2ee268a72c8a8676b2
vamprodo47 <vamprodo47@gmail.com>
2022-05-23 20:22:42 +0900

    Implement Wallet Page

--- client/src/pages/Wallet.js
+++ client/src/pages/Wallet.js
@@ -1,14 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import { Cookies } from "react-cookie";
 
-const Wallet = ({ userObjectId }) => {
+const Wallet = () => {
+  const cookies = new Cookies();
   const [user, setUser] = useState();
   const [tokenArray, setTokenArray] = useState([]); // ⭐️⭐️⭐️⭐️⭐️ token들을 map 사용해서 분리해줘야합니다.
+  const cookieUserType = cookies.get("userType");
+  const cookieUserObjectId = cookies.get("userObjectId");
 
   useEffect(() => {
     axios
-      .get(`http://localhost:4000/users/${userObjectId}`)
+      .get(`http://localhost:4000/users/${cookieUserObjectId}`)
       .then((res) => {
         const data = res.data;
 
@@ -32,9 +36,30 @@ const Wallet = ({ userObjectId }) => {
           </div>
         ) : (
           <div>
+            <br />
             {/* <div>My Token : {user.token}</div> */}
             <div>My Staking: {user.stakedToken}</div>
-            <div>My NFTs: {user.collectedNft}</div>
+            {cookieUserType === "2" ? (
+              <div>
+                My Custom-made NFT:
+                {user.customMadeNft.map((nft) => {
+                  return (
+                    <div key={nft._id}>
+                      <div>{nft.nftName}</div>
+                      <div>{nft.discountRate}</div>
+                      <div>{nft.nftPrice}</div>
+                    </div>
+                  );
+                })}
+              </div>
+            ) : (
+              <></>
+            )}
+            {cookieUserType === "1" ? (
+              <div>My NFTs: {user.collectedNft}</div>
+            ) : (
+              <></>
+            )}
           </div>
         )}
       </div>
