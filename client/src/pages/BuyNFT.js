a6b49039ba1a5ac137c24e9a46ac0e7fca0827ee
Robin Hanjun Kwon <khj02021@gmail.com>
2022-05-26 13:26:52 +0900

    Add NFT feature into Front-end

--- client/src/pages/BuyNFT.js
+++ client/src/pages/BuyNFT.js
@@ -1,21 +1,26 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Cookies } from "react-cookie";
 
 const BuyNFT = () => {
   const { restaurantObjectId } = useParams();
   const [nfts, setNfts] = useState([]);
-  const [selectedNft, setSelectedNft] = useState(""); // ⭐️⭐️⭐️⭐️⭐️ selectedNft는 발행해줘야할 nft 모델의 id입니다.
+  const [selectedNft, setSelectedNft] = useState({}); // ⭐️⭐️⭐️⭐️⭐️ selectedNft는 발행해줘야할 nft 모델의 id입니다.
+
+  const cookies = new Cookies();
 
   const onNftHandler = (e) => {
     e.preventDefault();
     setSelectedNft(e.currentTarget.value);
   };
 
-  const onBuyHandler = async (e) => {
-    let body = {}; // ⭐️⭐️⭐️⭐️⭐️ body에 보낼 키-값을 작성해줘야 합니다.
+  const onBuyHandler = async (nft) => {
+	const customerId = cookies.get("userObjectId");
+	let body = {...nft, restaurantObjectId: nft.user_id, customerId}; // ⭐️⭐️⭐️⭐️⭐️ body에 보낼 키-값을 작성해줘야 합니다.
 
-    // await axios.post("/nfts", body); // ⭐️⭐️⭐️⭐️⭐️ customer의 지갑으로 NFT가 민팅되는 것을 구현해야 합니다.
+	console.log(body);
+    await axios.post("/nfts", body); // ⭐️⭐️⭐️⭐️⭐️ customer의 지갑으로 NFT가 민팅되는 것을 구현해야 합니다.
   };
 
   useEffect(() => {
@@ -44,21 +49,13 @@ const BuyNFT = () => {
             <div>{nft.nftName}</div>
             <div>{nft.discountRate} (%)</div>
             <div>{nft.nftPrice} (DEDE)</div>
-            <input
-              type="radio"
-              name="nftInfo"
-              value={nft._id}
-              onChange={onNftHandler}
-            />
+			<button onClick={() => {onBuyHandler(nft)}}>
+        	  Buy
+        	</button>
           </div>
         );
       })}
       <br />
-      <p>
-        <button type="submit" value="Submit" onClick={onBuyHandler}>
-          Buy
-        </button>
-      </p>
     </>
   );
 };
