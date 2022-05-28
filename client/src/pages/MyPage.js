f6794600cc6318639648fb88ce799b7764ba26ec
vamprodo47 <vamprodo47@gmail.com>
2022-05-28 15:35:36 +0900

    Design Mypage component

--- client/src/pages/MyPage.js
+++ client/src/pages/MyPage.js
@@ -3,6 +3,71 @@ import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import SignIn from "./SignIn";
 import { Cookies } from "react-cookie";
+import styled from "styled-components";
+
+const StyledMain = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+const StyledH1 = styled.h1`
+  margin: 60px 0 15px 0;
+`;
+
+const StyledH3 = styled.h3`
+  margin: 20px 0 5px 0;
+`;
+
+const StyledSection1 = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+  height: 100%;
+  width: 380px;
+  margin-bottom: 20px;
+`;
+
+const StyledSection2 = styled.section`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  margin-bottom: 23px;
+  border-top: 1px solid rgb(0, 194, 189);
+`;
+
+const StyledLabel1 = styled.label`
+  width: 100%;
+  padding-right: 32px;
+`;
+
+const StyledLabel2 = styled.label`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+  margin-bottom: 8px;
+  border-top: 1px solid rgb(0, 194, 189);
+`;
+
+const StyledButton = styled.button`
+  position: absolute;
+  top: 105%;
+  left: 70%;
+  color: white;
+  background: #00c2bd;
+  padding: 0.375rem 0.75rem;
+  border: 1px solid #00c2bd;
+  border-radius: 10px;
+  width: 10em;
+`;
+
+const StyledLink = styled(Link)`
+  color: #00c2bd;
+  margin: 15px;
+`;
 
 const MyPage = ({ setIsSignedIn }) => {
   const cookies = new Cookies();
@@ -52,37 +117,37 @@ const MyPage = ({ setIsSignedIn }) => {
   return (
     <>
       {cookieIsSignedIn ? (
-        <div>
-          <div>
-            <br />
-            <br />
-            <div>User Info</div>
-            <br />
-            <div>User Name: {user.userName}</div>
-            <div>User Phone Number: {user.phoneNumber}</div>
-            <div>User Address: {user.roadNameAddress}</div>
-            {cookieUserType === "2" ? (
-              <div>
-                <Link to="/registration">Registration</Link>
-              </div>
-            ) : (
-              <></>
-            )}
-            <div>
-              <Link to="/history">My Order</Link>
-            </div>
-
-            <div>
-              <Link to="/wallet">My Wallet</Link>
-            </div>
-          </div>
-          <button onClick={signOutHandler}>Sign Out</button>
-        </div>
+        <StyledMain>
+          <StyledSection1>
+            <StyledH1>User Info</StyledH1>
+            <StyledSection2>
+              <StyledLabel1>
+                <StyledH3>User Name</StyledH3>
+                {user.userName}
+              </StyledLabel1>
+              <StyledLabel1>
+                <StyledH3>User Phone Number</StyledH3>
+                {user.phoneNumber}
+              </StyledLabel1>
+              <StyledLabel1>
+                <StyledH3>User Address</StyledH3>
+                {user.roadNameAddress}
+              </StyledLabel1>
+            </StyledSection2>
+            <StyledLabel2>
+              {cookieUserType === "2" ? (
+                <StyledLink to="/registration">Registration</StyledLink>
+              ) : (
+                <></>
+              )}
+              <StyledLink to="/history">My Order</StyledLink>
+              <StyledLink to="/wallet">My Wallet</StyledLink>
+            </StyledLabel2>
+            <StyledButton onClick={signOutHandler}>Sign Out</StyledButton>
+          </StyledSection1>
+        </StyledMain>
       ) : (
-        <div>
-          <br />
-          <Link to="/signin">Please Sign In</Link>
-        </div>
+        <StyledLink to="/signin">Please Sign In</StyledLink>
       )}
     </>
   );
