1efc3bc00d1e2b0b954da22a4df504bf91a47160
Robin Hanjun Kwon <khj02021@gmail.com>
2022-05-30 07:14:56 +0900

    Finish styling

--- client/src/pages/BuyNFT.js
+++ client/src/pages/BuyNFT.js
@@ -2,6 +2,57 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Cookies } from "react-cookie";
+import styled from 'styled-components';
+
+const StyledMain = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+const StyledH1 = styled.h1`
+  margin: 60px 0 30px 0;
+`;
+
+const StyledH2 = styled.h2`
+  margin-bottom: 20px;
+`;
+
+const StyledSection1 = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100%;
+  width: 480px;
+  border-top: 1px solid rgb(0, 194, 189);
+`;
+
+const StyledSection2 = styled.section`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  margin: 40px 0 0 0;
+  padding: 26px;
+  border: 1px solid rgb(0, 194, 189);
+  border-radius: 20px;
+  position: relative;
+`;
+
+const StyledLabel = styled.label`
+  margin-bottom: 3px;
+`;
+
+const StyledButton = styled.button`
+  color: white;
+  background: #00c2bd;
+  padding: 0.375rem 0.75rem;
+  border: 1px solid #00c2bd;
+  border-radius: 10px;
+  width: 10em;
+  height: 30px;
+  cursor: pointer;
+`;
 
 const BuyNFT = () => {
   const { restaurantObjectId } = useParams();
@@ -11,56 +62,57 @@ const BuyNFT = () => {
   const cookies = new Cookies();
 
   const onNftHandler = (e) => {
-    setSelectedNft(e.currentTarget.value);
+	setSelectedNft(e.currentTarget.value);
   };
 
   const onBuyHandler = async (nft) => {
-    const customerId = cookies.get("userObjectId");
-    let body = { ...nft, restaurantObjectId: nft.user_id, customerId }; // ⭐️⭐️⭐️⭐️⭐️ body에 보낼 키-값을 작성해줘야 합니다.
+	const customerId = cookies.get("userObjectId");
+	let body = { ...nft, restaurantObjectId: nft.user_id, customerId }; // ⭐️⭐️⭐️⭐️⭐️ body에 보낼 키-값을 작성해줘야 합니다.
 
-    console.log(body);
+	console.log(body);
 
-    await axios.post("/nfts", body); // ⭐️⭐️⭐️⭐️⭐️ customer의 지갑으로 NFT가 민팅되는 것을 구현해야 합니다.
+	await axios.post("/nfts", body); // ⭐️⭐️⭐️⭐️⭐️ customer의 지갑으로 NFT가 민팅되는 것을 구현해야 합니다.
   };
 
   useEffect(() => {
-    axios
-      .get(`/restaurants/${restaurantObjectId}/nfts`)
-      .then((res) => {
-        const data = res.data.nftList;
+	axios
+	  .get(`/restaurants/${restaurantObjectId}/nfts`)
+	  .then((res) => {
+		const data = res.data.nftList;
 
-        console.log("data:", data);
+		console.log("data:", data);
 
-        setNfts(data);
-      })
-      .catch((error) => {
-        console.log("❌ Client Get Restaurant NFTs Error:", error);
-      });
+		setNfts(data);
+	  })
+	  .catch((error) => {
+		console.log("❌ Client Get Restaurant NFTs Error:", error);
+	  });
   }, []);
 
   return (
-    <>
-      <br />
-      NFT List
-      {nfts.map((nft) => {
-        return (
-          <div key={nft._id}>
-            <br />
-            <div>{nft.nftName}</div>
-            <div>{nft.discountRate} (%)</div>
-            <div>{nft.nftPrice} (DEDE)</div>
-            <button
-              onClick={() => {
-                onBuyHandler(nft);
-              }}
-            >
-              Buy
-            </button>
-          </div>
-        );
-      })}
-      <br />
-    </>
+	<StyledMain>
+	  <StyledH1>
+		NFT List
+	  </StyledH1>
+	  <StyledSection1>
+		{nfts.map((nft) => {
+		  return (
+			<StyledSection2 key={nft._id}>
+			  <StyledH2>{nft.nftName}</StyledH2>
+			  <StyledLabel>{nft.discountRate} (%)</StyledLabel>
+			  <StyledLabel>{nft.nftPrice} (DEDE)</StyledLabel>
+			  <StyledButton
+				onClick={() => {
+				  onBuyHandler(nft);
+				}}
+			  >
+				Buy
+			  </StyledButton>
+			</StyledSection2>
+		  );
+		})}
+	  </StyledSection1>
+	</StyledMain>
   );
 };
 
--- client/src/pages/Pending.js
+++ client/src/pages/Pending.js
@@ -2,6 +2,69 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Cookies } from "react-cookie";
+import styled from 'styled-components';
+
+const StyledMain = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+const StyledH1 = styled.h1`
+  margin: 60px 0 30px 0;
+`;
+
+const StyledSection1 = styled.section`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  width: 480px;
+  border-top: 1px solid rgb(0, 194, 189);
+  margin-bottom: 15px;
+`;
+
+const StyledButton = styled.button`
+  color: white;
+  background: #00c2bd;
+  padding: 0.375rem 0.75rem;
+  border: 1px solid #00c2bd;
+  border-radius: 10px;
+  width: 10em;
+  height: 30px;
+`;
+
+const StyledLabel = styled.label`
+  margin-bottom: 3px;
+`;
+
+const StyledSection2 = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  margin: 40px 0 0 0;
+  padding: 30px;
+  border: 1px solid rgb(0, 194, 189);
+  border-radius: 20px;
+`;
+
+const StyledLabel3 = styled.label``;
+
+const StyledRadio = styled.input`
+  margin-left: 4px;
+`;
+
+const StyledH2 = styled.h2`
+  margin-bottom: 3px;
+`;
+
+const StyledLabel2 = styled.label`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  margin-bottom: 8px;
+`;
 
 const Pending = () => {
   const cookies = new Cookies();
@@ -13,118 +76,129 @@ const Pending = () => {
   const cookieUserObjectId = cookies.get("userObjectId");
 
   const onStatusHandler = (e) => {
-    setOrderObjectId(e.currentTarget.name);
-    setStatus(e.currentTarget.value);
+	setOrderObjectId(e.currentTarget.name);
+	setStatus(e.currentTarget.value);
   };
 
   const onSubmitHandler = async () => {
 	let body = { deliveryManObjectId: cookieUserObjectId, status };
 
-    await axios.patch(`/orders/${orderObjectId}`, body).then((res) => {
-      const data = res.data;
+	await axios.patch(`/orders/${orderObjectId}`, body).then((res) => {
+	  const data = res.data;
 
-      console.log(data.message);
-    });
+	  console.log(data.message);
+	});
   };
 
   useEffect(() => {
-    if (cookieUserObjectId) {
-      axios
-        .get(`/orders`)
-        .then(async (res) => {
-          let pendingOrder = [];
+	if (cookieUserObjectId) {
+	  axios
+		.get(`/orders`)
+		.then(async (res) => {
+		  let pendingOrder = [];
 
-          res.data.orderList.map((order) => {
-            if (order.status === "Cooking") {
-              pendingOrder.push(order);
-            }
-          });
+		  res.data.orderList.map((order) => {
+			if (order.status === "Cooking") {
+			  pendingOrder.push(order);
+			}
+		  });
 
-          console.log("pendingOrder:", pendingOrder);
+		  console.log("pendingOrder:", pendingOrder);
 
-          setPending(pendingOrder);
-        })
-        .catch((error) => {
-          console.log("❌ Client Get Pending Page Error:", error);
-        });
-    }
+		  setPending(pendingOrder);
+		})
+		.catch((error) => {
+		  console.log("❌ Client Get Pending Page Error:", error);
+		});
+	}
   }, [cookieUserObjectId]);
 
   return (
-    <div>
-      <br />
-      <br />
-      <div>Pending Order</div>
-      <br />
-      {pending.length === 0 ? <div>❗️ No Pending Order</div> : <></>}
-      {pending.map((order) => {
-        return (
-          <div key={order._id}>
-            <div>Status</div>
-            <div>{order.status}</div>
-            <div>Customer</div>
-            <div>{order.user1_id.userName}</div>
-			<div>Customer Address</div>
-			<div>{order.user1_id.roadNameAddress}</div>
-            <div>Restaurant</div>
-            <div>{order.user2_id.userName}</div>
-			<div>Restaurant Address</div>
-			<div>{order.user2_id.roadNameAddress}</div>
-			<div>Restaurant Staked Token</div>
-			<div>{order.user2_id.stakedToken}</div>
-			<div>Distance between Customer and Restaurant</div>
-			<div>{order.distance}</div>
-            <div>Delivery Man</div>
-            <div>
-              {order.user3_id
-                ? order.user3_id.userName
-                : "🚴‍♂️ You can deliver it!"}
-            </div>
-            <div>Ordered Menu</div>
-            <div>
-              {order.orderedMenu.map((menuInfo, index) => {
-                return (
-                  <div key={index}>
-                    <br />
-                    <div>Name</div>
-                    <div>{menuInfo.menuName}</div>
-                    <div>Description</div>
-                    <div>{menuInfo.menuDescription}</div>
-                    <div>Price</div>
-                    <div>{menuInfo.menuPrice} (DEDE)</div>
-                  </div>
-                );
-              })}
-            </div>
-            <br />
-            {cookieUserType === "3" ? (
-              <div>
-                <input
-                  type="radio"
-                  name={order._id}
-                  value="Delivery"
-                  onClick={onStatusHandler}
-                />
-                Delivery
-              </div>
-            ) : (
-              <></>
-            )}
-          </div>
-        );
-      })}
-      {cookieUserType === "3" ? (
-        <div>
-          <br />
-          <button onClick={onSubmitHandler}>Submit</button>
-        </div>
-      ) : (
-        <div>
-          <br />
-          <Link to="/signin">Please Sign In</Link>
-        </div>
-      )}
-    </div>
+	<StyledMain>
+	  <StyledH1>Pending Order</StyledH1>
+	  {pending.length === 0 ? <div>❗️ No Pending Order</div> : <></>}
+	  <StyledSection1>
+		{pending.map((order) => {
+		  return (
+			<StyledSection2 key={order._id}>
+			  <StyledLabel2>
+				<StyledH2>
+				  Status
+				</StyledH2>
+				{order.status}
+			  </StyledLabel2>
+			  <StyledLabel2>
+				<StyledH2>
+				  Customer
+				</StyledH2>
+				{order.user1_id.userName}
+			  </StyledLabel2>
+			  <StyledLabel2>
+				<StyledH2>
+				  Customer Address
+				</StyledH2>
+				{order.user1_id.roadNameAddress}
+			  </StyledLabel2>
+			  <StyledLabel2>
+				<StyledH2>
+				  Restaurant
+				</StyledH2>
+				{order.user2_id.userName}
+			  </StyledLabel2>
+			  <StyledLabel2>
+				<StyledH2>
+				  Restaurant Address
+				</StyledH2>
+				{order.user2_id.roadNameAddress}
+			  </StyledLabel2>
+			  <StyledLabel2>
+				<StyledH2>
+				  Restaurant Staked Token
+				</StyledH2>
+				{order.user2_id.stakedToken}
+			  </StyledLabel2>
+			  <StyledLabel2>
+				<StyledH2>
+				  Distance between Customer and Restaurant
+				</StyledH2>
+				{order.distance} (m)
+			  </StyledLabel2>
+			  <StyledH2>Ordered Menu</StyledH2>
+			  {order.orderedMenu.map((menuInfo, index) => {
+				return (
+				  <StyledSection2 key={index}>
+					<StyledH2>{menuInfo.menuName}</StyledH2>
+					<StyledLabel>{menuInfo.menuDescription}</StyledLabel>
+					<StyledLabel>{menuInfo.menuPrice} (DEDE)</StyledLabel>
+				  </StyledSection2>
+				);
+			  })}
+			  {cookieUserType === "3" ? (
+				<StyledLabel3>
+				  <StyledRadio
+					type="radio"
+					name={order._id}
+					value="Delivery"
+					onClick={onStatusHandler}
+				  />
+				  Delivery
+				</StyledLabel3>
+			  ) : (
+				<></>
+			  )}
+			</StyledSection2>
+		  );
+		})}
+	  </StyledSection1>
+	  {cookieUserType === "3" ? (
+		<StyledButton onClick={onSubmitHandler}>Submit</StyledButton>
+	  ) : (
+		<div>
+		  <br />
+		  <Link to="/signin">Please Sign In</Link>
+		</div>
+	  )}
+	</StyledMain>
   );
 };
 
